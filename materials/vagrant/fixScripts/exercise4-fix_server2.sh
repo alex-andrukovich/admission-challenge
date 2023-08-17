032e4041bfe35b8b2239366803f31b23cb142006
alex-andrukovich <142259407+alex-andrukovich@users.noreply.github.com>
2023-08-17 21:24:37 +0300

    Update exercise4-fix_server2.sh

--- materials/vagrant/fixScripts/exercise4-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-#add fix to exercise4-server2 here
+
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+
+if ! (grep -q server1 /etc/hosts); then
+   sed -i '$ a 192.168.60.10 server1' /etc/hosts
+fi
+if ! (grep -q server1 /etc/ssh/ssh_config); then
+  sed -i '$ a Host server1\n    StrictHostKeyChecking no\n' /etc/ssh/ssh_config
+fi
+
+systemctl restart sshd
