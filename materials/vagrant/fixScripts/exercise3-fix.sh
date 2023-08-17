800674a63ff35463e0a8a9f3835f576477bc5667
alex-andrukovich <142259407+alex-andrukovich@users.noreply.github.com>
2023-08-17 21:24:02 +0300

    Update exercise3-fix.sh

--- materials/vagrant/fixScripts/exercise3-fix.sh
+++ materials/vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-#add fix to exercise3 here
+sed -i 's/Require all denied/Require all granted/g' /etc/apache2/sites-available/000-default.conf
+chown -R www-data:www-data /var/www/
+systemctl restart apache2
